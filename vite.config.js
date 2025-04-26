fde6fa17165135850eb42746531da555fdd06ffd
AndresColorado9371 <fa.c@lanuevaamerica.edu.co>
2025-04-25 21:00:36 -0500

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/aplicacionvivero.github.io",
+  base: "/src/App.jsx",
   plugins: [react()],
 })
